641f57b7f2e7fff75bd67d647cf0e70a9814a1c4
Colibridev <54209311+Fifaliana-tech@users.noreply.github.com>
2022-04-28 17:49:55 +0300

    enlever les commentaires

--- my-first-js-programs/add.js
+++ my-first-js-programs/add.js
@@ -1,6 +1,6 @@
+console.log(2 + 2); //4
+
 // console.log("J'aime apprendre le Javascript");
 
 // var nom = "RAKOTOARIMAHA "; prenom = "Précieuse";
 // console.log(`${nom} ${prenom} aime apprendre le JavaScript`);
-
-// console.log(2 + 2);
