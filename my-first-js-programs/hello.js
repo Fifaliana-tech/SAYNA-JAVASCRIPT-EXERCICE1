04f8edab50eb95f9d614aae2a0b9371c4a2f66a8
Colibridev <54209311+Fifaliana-tech@users.noreply.github.com>
2022-04-28 17:18:55 +0300

    first commit

---
+++ my-first-js-programs/hello.js
@@ -0,0 +1,68 @@
+// Leçon n°1 : HELLO WORD
+
+// console.log('hello world');
+//----------------------------------------------------------------
+
+// Leçon n°2 : VARIABLES AND TYPES
+
+// let myNumber=4;
+// let myString="Variables are great.";
+// let myBoolean=false;
+// console.log("myNumber is equal to " + myNumber);
+// console.log("myString is equal to " + myString);
+// console.log("myBoolean is equal to " + myBoolean)
+
+//-----------------------------------
+// Leçon n°3 : ARRAYS
+
+// let monTableau = ["Quel est le sens de la vie ?", 42, true];
+// console.log(monTableau);
+
+//----------------------------------------------------
+// MANIPULATING ARRAYS
+
+// let myStack = [];
+// myStack.push(1);
+// myStack.push(2);
+// myStack.push(3);
+// console.log(myStack);
+// console.log(myStack.pop()); 
+// console.log(myStack); 
+
+//----------------------------------------------------
+//PUSH & POP
+// var myQueue = [] 
+// myQueue.push(1); 
+// myQueue.push(2); 
+// myQueue.push(3); 
+ 
+// console.log(myQueue.shift()); 
+// console.log(myQueue.shift()); 
+// console.log(myQueue.shift());
+
+//-----------------------------------
+//UNSHIFT
+// let myArray = [1, 2, 3];
+// myArray.unshift(0);
+// console.log(myArray);
+
+//---------------------------
+// let myArray =[0,1,2,3,4,5,6,7,8,9]; 
+// let splice = myArray.splice(3,5); 
+ 
+// console.log(splice); //imprimera 3,4,5,6,7 
+// console.log(myArray); //le résultat sera 0,1,2,8,9 
+
+
+//-----------------------------------
+// let monTableau = [true, "Quel est le sens de la vie ?"];
+// monTableau.push(42);
+// monTableau.shift();
+
+// console.log(monTableau[0]); 
+// console.log(monTableau[1]); 
+
+//--------------------------------------
+// let a =1; 
+// let b =2; 
+// let c = a+b;
